046327b77913fcf23822885a1ef98e2ccfb0371d
k-prog55 <keianzai@keinoMacBook-Air.local>
2023-07-15 14:50:32 +0900

    小文字に変換して標準出力

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         List<String> names = List.of("yamada","takana","yasuda","suzuki","iida");
         //名前を大文字に変換して標準出力する
-        names.stream().map(name -> name.toUpperCase()).forEach(name -> System.out.println(name));
+        names.stream().map(String::toUpperCase).forEach(System.out::println);
         //名前を大文字に変換して標準出力する（メソッド変換で記載、内容は同じ、クラス::メソッドの形で書く）
         names.stream().map(String::toUpperCase).forEach(System.out::println);
+        //名前を小文字に変換して標準出力する
+        names.stream().map(String::toLowerCase).forEach(System.out::println);
     }
 }
